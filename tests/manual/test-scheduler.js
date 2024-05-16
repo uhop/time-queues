935157369e45b6a4cdf7991d37993c3406ae8d50
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-05-16 16:38:11 -0500

    Made the test faster.

--- tests/manual/test-scheduler.js
+++ tests/manual/test-scheduler.js
@@ -11,27 +11,27 @@ const ping = id => (task, scheduler) => {
 
 console.log('Multiple pings. Exit with Ctrl-C.');
 
-scheduler.enqueue(1000, ping(1));
-scheduler.enqueue(1000, ping(2));
-scheduler.enqueue(3000, ping(3));
-scheduler.enqueue(5000, ping(5));
+scheduler.enqueue(100, ping(1));
+scheduler.enqueue(100, ping(2));
+scheduler.enqueue(300, ping(3));
+scheduler.enqueue(500, ping(5));
 
-scheduler.enqueue(10000, (_, scheduler) => {
-  console.log(`Now we stop the queue for 5s at ${(Date.now() - startTime) / 1000}s...`);
+scheduler.enqueue(600, (_, scheduler) => {
+  console.log(`Now we stop the queue for 0.4s at ${(Date.now() - startTime) / 1000}s...`);
   scheduler.pause();
-  setTimeout(() => (console.log('...starting again'), scheduler.resume()), 5000);
+  setTimeout(() => (console.log('...starting again'), scheduler.resume()), 400);
 });
 
-scheduler.enqueue(20000, (task, scheduler) => {
+scheduler.enqueue(1500, (task, scheduler) => {
   console.log(`Now we stop the queue for good at ${(Date.now() - startTime) / 1000}s...`);
   scheduler.pause().clear();
 });
 
-const prohibitedTask = scheduler.enqueue(3000, () => {
+const prohibitedTask = scheduler.enqueue(300, () => {
   throw new Error('Should not be called');
 });
 
-scheduler.enqueue(2000, (_, scheduler) => {
+scheduler.enqueue(200, (_, scheduler) => {
   console.log(`We are removing the prohibited task at ${(Date.now() - startTime) / 1000}s...`);
   scheduler.dequeue(prohibitedTask);
 });

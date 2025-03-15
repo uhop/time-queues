10802b00bbab446bde2dea515af62f317349560e
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-03-15 10:39:36 -0500

    Added tests for time functions.

---
+++ tests/test-audit.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import test from 'tape-six';
+
+import audit from '../src/audit.js';
+import sleep from '../src/sleep.js';
+
+test('audit', async t => {
+  t.equal(typeof audit, 'function');
+
+  let results = [];
+  const fn = x => results.push(x),
+    auditedFn = audit(fn, 20);
+
+  auditedFn(1);
+  t.deepEqual(results, []);
+
+  await sleep(10);
+  auditedFn(2);
+  t.deepEqual(results, []);
+
+  await sleep(20);
+  t.deepEqual(results, [2]);
+});
---
+++ tests/test-throttle.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import test from 'tape-six';
+
+import throttle from '../src/throttle.js';
+import sleep from '../src/sleep.js';
+
+test('throttle', async t => {
+  t.equal(typeof throttle, 'function');
+
+  let results = [];
+  const fn = x => results.push(x),
+    throttledFn = throttle(fn, 20);
+
+  throttledFn(1);
+  t.deepEqual(results, [1]);
+  
+  await sleep(10);
+  throttledFn(2);
+  t.deepEqual(results, [1]);
+
+  results = [];
+  await sleep(20);
+  t.deepEqual(results, []);
+});

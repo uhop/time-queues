2f4ad26d00042e5a87847cf86e60da59f7006b0a
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-03-15 10:38:49 -0500

    Removed unnecessary `if`, fixed the initial timeout.

--- src/sample.js
+++ src/sample.js
@@ -3,19 +3,19 @@
 export const sample = (fn, ms) => {
   const started = Date.now();
   let handle = null,
-    lastSeenArgs = null;
+    lastSeenArgs = null,
+    timeout = ms;
   return (...args) => {
     lastSeenArgs = [...args];
     if (handle) return;
     const diff = (Date.now() - started) % ms,
-      delay = diff ? ms - diff : 0;
+      delay = diff ? ms - diff : timeout;
+    timeout = 0;
     handle = setTimeout(() => {
       handle = null;
-      if (lastSeenArgs) {
-        const args = lastSeenArgs;
-        lastSeenArgs = null;
-        fn(...args);
-      }
+      const args = lastSeenArgs;
+      lastSeenArgs = null;
+      fn(...args);
     }, delay);
   };
 };

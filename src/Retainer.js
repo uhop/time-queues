4934c3e352de2c375810c12535d92de4a90c5266
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-03-14 23:50:13 -0500

    Added `Retainer`.

---
+++ src/Retainer.js
@@ -0,0 +1,46 @@
+'use strict';
+
+export class Retainer {
+  constructor({create, destroy, retentionPeriod = 1_000}) {
+    if (!create || !destroy) throw new Error('Retainer: create and destroy are required');
+    this.create = create;
+    this.destroy = destroy;
+    this.retentionPeriod = retentionPeriod;
+    this.counter = 0;
+    this.handle = null;
+    this.value = null;
+  }
+
+  async get() {
+    if (!this.counter) {
+      if (this.handle) {
+        clearTimeout(this.handle);
+        this.handle = null;
+      } else {
+        this.value = await this.create();
+      }
+    }
+    ++this.counter;
+    return this.value;
+  }
+
+  async release(immediately) {
+    if (this.counter <= 0) throw new Error('Retainer: counter is already zero');
+    if (--this.counter) return this;
+    if (immediately) {
+      const value = this.value;
+      this.value = null;
+      await this.destroy(value);
+      return this;
+    }
+    this.handle = setTimeout(async () => {
+      const value = this.value;
+      this.value = null;
+      this.handle = null;
+      await this.destroy(value);
+    }, this.retantionPeriod);
+    return this;
+  }
+}
+
+export default Retainer;

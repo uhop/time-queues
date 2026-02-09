d427dc002d70e7a52e316cd5d507f30ad261481d
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2026-02-08 22:03:41 -0600

    Copied TS tests to JS tests.

---
+++ tests/test-random-sleep.js
@@ -0,0 +1,38 @@
+import test from 'tape-six';
+
+import {
+  randomUniformSleep,
+  randomNormalSleep,
+  randomExpoSleep,
+  randomParetoSleep,
+  randomSleep
+} from '../src/random-sleep.js';
+
+test('random sleep', async t => {
+  t.equal(typeof randomUniformSleep, 'function');
+  const u = randomUniformSleep(5, 15);
+  t.equal(typeof u, 'function');
+  await u();
+  await u();
+
+  t.equal(typeof randomNormalSleep, 'function');
+  const n = randomNormalSleep(15, 5);
+  t.equal(typeof n, 'function');
+  await n();
+  await n();
+
+  t.equal(typeof randomExpoSleep, 'function');
+  const e = randomExpoSleep(0.1);
+  t.equal(typeof e, 'function');
+  await e();
+  await e();
+
+  t.equal(typeof randomParetoSleep, 'function');
+  const p = randomParetoSleep(5, 1.161);
+  t.equal(typeof p, 'function');
+  await p();
+  await p();
+
+  t.equal(typeof randomSleep, 'function');
+  await randomSleep(20, 10);
+});

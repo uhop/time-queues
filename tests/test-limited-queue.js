2f2e477c53d25771cf66e119ddc7fe94ee0292a8
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2026-02-10 14:41:18 -0600

    Added synchronous tests.

--- tests/test-limited-queue.js
+++ tests/test-limited-queue.js
@@ -3,7 +3,7 @@ import test from 'tape-six';
 import {LimitedQueue} from '../src/LimitedQueue.js';
 import randomSleep from '../src/random-sleep.js';
 
-test('LimitedQueue', async t => {
+test('LimitedQueue with random sleep', async t => {
   t.equal(typeof LimitedQueue, 'function');
 
   let counter = 0,
@@ -32,4 +32,32 @@ test('LimitedQueue', async t => {
 
   t.equal(counter, 0);
   t.equal(maxCount, M);
+});
+
+test('LimitedQueue with sync tasks', async t => {
+  let counter = 0,
+    maxCount = 0;
+
+  const fn = i => () => {
+    ++counter;
+    maxCount = Math.max(maxCount, counter);
+    --counter;
+    return i;
+  };
+
+  const fs = [],
+    N = 20,
+    M = 3;
+
+  const queue = new LimitedQueue(M);
+
+  for (let i = 0; i < N; ++i) {
+    const task = queue.schedule(fn(i));
+    fs.push(task.promise);
+  }
+
+  await Promise.all(fs);
+
+  t.equal(counter, 0);
+  t.ok(maxCount <= M);
 });
